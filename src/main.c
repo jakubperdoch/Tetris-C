d424764392a532eaeaeef3a60593f62af1ee720c
Jakub Perdoch <perdochjakub@gmail.com>
2025-12-26 20:31:01 +0100

    add initial implementation of Tetris game with SDL2 integration

--- src/main.c
+++ src/main.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
+#include <SDL.h>
 
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 800
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
+    if (SDL_Init(SDL_INIT_VIDEO) )
+    {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window)
+    {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer)
+    {
+        printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+    }
+
+    int running = 1;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 20, 20, 30, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
